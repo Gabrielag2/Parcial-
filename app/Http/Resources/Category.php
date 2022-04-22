729fe9a5ac85bd68b744e75b397d89f9da84b519
gabyag <gabyaguil915@gmail.com>
2022-04-22 12:14:15 -0600

    Cambios de parcial semana 14

---
+++ app/Http/Resources/Category.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Category extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        //return parent::toArray($request);
+        return [
+            'id'=>$this->id,
+            'title'=>$this->title,
+            'Tradiciones'=>$this->tradiciones,
+            'Nombre'=>$this->nombre,
+            'description'=>$this->description,
+            'Precio'=>$this->precio,
+        
+        ];
+    }
+}
---
+++ database/migrations/2022_04_08_190958_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('tradidiones');
+             $table->string('description');
+            $table->string('nombre');
+            $table->string('description');
+            $table->integer('precio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
